75f1d6c1fa7f203f9d8e075472b4ce2fe56556ad
YejinHwang-D <hyj3463@naver.com>
2021-04-26 17:54:47 +0900

    1028~1037

---
+++ 1030_18639363(AC).c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main() {
+	long long int n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+	return 0;
+}
---
+++ 1035_18640133(AC).c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%x", &n);
+	printf("%o", n);
+	return 0;
+}
