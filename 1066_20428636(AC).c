bf6a1e9cc327030d50e611190ac7c876b7c56d8b
YejinHwang-D <hyj3463@naver.com>
2021-07-18 15:19:14 +0900

    1063~1070

---
+++ 1066_20428636(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	int arr[3];
+	scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
+	
+	for(int i=0; i<3; i++) {
+	    if (arr[i]%2==0) {
+	        printf("even\n");
+	    }
+	    else {
+	        printf("odd\n");
+	    }
+	}
+	
+	return 0;
+}
---
+++ 1069_20428789(AC).c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main() {
+	char grade;
+	scanf("%c", &grade);
+	switch (grade) {
+	    case 'A':
+	        printf("best!!!\n");
+	        break;
+	    case 'B':
+	        printf("good!!\n");
+	        break;
+	    case 'C':
+	        printf("run!\n");
+	        break;
+	    case 'D':
+	        printf("slowly~\n");
+	        break;
+	   default:
+	        printf("what?\n");
+	        break;
+	}
+	return 0;
+}
