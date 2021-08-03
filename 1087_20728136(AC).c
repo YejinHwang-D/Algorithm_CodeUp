da22a5b6b2037ec8723b9eafbd7c229b4748b43d
YejinHwang-D <hyj3463@naver.com>
2021-08-03 21:13:25 +0900

    1086~1087

---
+++ 1087_20728136(AC).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+	int num, sum=0, i=0;
+	scanf("%d", &num);
+	
+	while (1) {
+	    sum += ++i;
+	    
+	    if (sum >= num) {
+	        printf("%d", sum);
+	        break;
+	    }
+	}
+	return 0;
+}
