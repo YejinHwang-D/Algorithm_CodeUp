a45ebc3c54e5b5a1e48c99a728931273bd407f2e
YejinHwang-D <hyj3463@naver.com>
2021-08-08 00:10:12 +0900

    1092~1093

---
+++ 1093_20769441(AC).c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n, num;
+	int arr[23] = {};
+	scanf("%d", &n);
+	for (int i=0; i<n; i++) {
+	    scanf("%d", &num);
+	    arr[num] = arr[num] + 1;
+	}
+	for (int i=1; i<=23; i++)
+	    printf("%d ", arr[i]);
+	return 0;
+}
