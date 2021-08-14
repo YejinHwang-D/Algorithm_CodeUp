2d358e1a183a2c095c68831a07fb88ea38052368
YejinHwang-D <hyj3463@naver.com>
2021-08-14 20:59:28 +0900

    1095~1098

---
+++ 1096_20837993(AC).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int n, x, y, arr[20][20]={0,};
+	scanf("%d", &n);
+	
+	for (int i=0; i<n; i++) {    //그냥 n번 반복하라는거
+	    scanf("%d %d", &x, &y);
+	    arr[x][y] = 1;
+	}
+	
+	for (int i=1; i<=19; i++) {
+	    for (int j=1; j<=19; j++)
+	        printf("%d ", arr[i][j]);
+	    printf("\n");
+	}
+	return 0;
+}
---
+++ 1097_20838372(AC).c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i,j, arr[20][20], n, x, y; 
+	for (i=1; i<=19; i++){
+	    for (j=1; j<=19; j++)
+	        scanf("%d", &arr[i][j]);
+	}
+	
+	scanf("%d", &n);
+	for (i=0; i<n; i++) {
+	    scanf("%d %d", &x, &y);
+	    for (j=1; j<=19; j++) {
+	        if (arr[x][j] == 1)
+	            arr[x][j] = 0;
+	        else
+	            arr[x][j] = 1;
+	    }
+	    for (j=1; j<=19; j++) {
+	        if (arr[j][y] == 1)
+	            arr[j][y] = 0;
+	        else
+	            arr[j][y] = 1;
+	    }
+	}
+	
+	for (i=1; i<=19; i++){
+	    for (j=1; j<=19; j++)
+	        printf("%d ", arr[i][j]);
+	    printf("\n");
+	}
+	
+	return 0;
+}
